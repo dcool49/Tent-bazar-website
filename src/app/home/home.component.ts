fc410948a781fde0d17556670b20aae4399b6f51
Chetan <chetan@cirkledin.com>
2025-09-08 20:37:43 +0530

    about us page added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,22 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { OfferServiceComponent } from '../landing-sections/offer-service/offer-service.component';
 import { HeroSectionComponent } from '../landing-sections/hero-section/hero-section.component';
 import { MobileSectionComponent } from '../landing-sections/mobile-section/mobile-section.component';
 // import { TabServiceSectionComponent } from '../landing-sections/tab-service-section/tab-service-section.component';
 // import { TestimonialComponent } from '../landing-sections/testimonial/testimonial.component';
 import { FindSectionComponent } from '../landing-sections/find-section/find-section.component';
-import { FooterComponent } from '../footer/footer.component';
 import { ReelsSectionComponent } from '../landing-sections/reels-section/reels-section.component';
 import { YoutubeSectionComponent } from '../landing-sections/youtube-section/youtube-section.component';
 
 @Component({
   selector: 'app-home',
   imports: [
-    HeaderComponent,
     HeroSectionComponent,
     OfferServiceComponent,
     MobileSectionComponent,
     // TabServiceSectionComponent,
     // TestimonialComponent,
     FindSectionComponent,
-    FooterComponent,
     ReelsSectionComponent,
     YoutubeSectionComponent
   ], // 👈 add here
