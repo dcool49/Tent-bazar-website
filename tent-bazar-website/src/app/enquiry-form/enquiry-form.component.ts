5ded076fb1a098fff84ee7ab929470911e666e59
Chetan <chetan@cirkledin.com>
2025-09-18 15:31:43 +0530

    mobile view changes

---
+++ tent-bazar-website/src/app/enquiry-form/enquiry-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enquiry-form',
+  imports: [],
+  templateUrl: './enquiry-form.component.html',
+  styleUrl: './enquiry-form.component.scss'
+})
+export class EnquiryFormComponent {
+
+}
