e2dda74354aa935f01cd12f732a6f88849ac996d
dcool49 <42763345+dcool49@users.noreply.github.com>
2025-10-09 23:25:10 +0530

    pagination added

--- tent-bazar-website/src/app/tent-admin/admin-products/admin-products.component.ts
+++ tent-bazar-website/src/app/tent-admin/admin-products/admin-products.component.ts
@@ -5,10 +5,10 @@ import { CommonModule } from '@angular/common';
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteConfirmModalComponent } from '../../data-model/delete-confirm-modal/delete-confirm-modal.component';
 import { ProductDetailsComponent } from '../../product-details/product-details.component';
-
+import { NgxPaginationModule } from 'ngx-pagination';
 @Component({
   selector: 'app-admin-products',
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   templateUrl: './admin-products.component.html',
   styleUrl: './admin-products.component.scss',
 })
@@ -16,7 +16,14 @@ export class AdminProductsComponent implements OnInit {
   productList: any;
   searchName = '';
   category: any;
-  selectedID:any;
+  selectedID: any;
+  p: number = 1; // Current page
+  itemsPerPage: number = 10; // Items per page
+
+  // Optional: Function to handle page changes
+  pageChanged(event: any) {
+    this.p = event;
+  }
   constructor(
     private route: Router,
     private dataService: DataService,
@@ -56,52 +63,57 @@ export class AdminProductsComponent implements OnInit {
   getcatName(id: any): String {
     if (this.category.length > 1) {
       const index = this.category.findIndex((val: any) => id === val._id);
-      if(index !== -1){
+      if (index !== -1) {
         return this.category[index]?.categoryName;
-      }else{
-        return '-'
+      } else {
+        return '-';
       }
     } else {
       return '-';
     }
   }
 
-  deleteProduct(id:any){
-    const url = 'product/delete?_id='+id;
-    this.dataService.deleteAPICall(url).subscribe((res:any)=>{
-      console.info(res);
-      this.getProductList();
-    },err=>{
-      console.error(err);
-    })
+  deleteProduct(id: any) {
+    const url = 'product/delete?_id=' + id;
+    this.dataService.deleteAPICall(url).subscribe(
+      (res: any) => {
+        console.info(res);
+        this.getProductList();
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
   }
 
-    openDialog(prod:any): void {
-      this.selectedID = prod._id
-      const dialogRef = this.dialog.open(DeleteConfirmModalComponent, {
-        width: '500px',
-        data: {heading:'Delete Product',msg:'Are you sure you want to delete',name:prod.productName}
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed',result);
-        if(result){
-          this.deleteProduct(this.selectedID);
-        }
-      });
-    }
+  openDialog(prod: any): void {
+    this.selectedID = prod._id;
+    const dialogRef = this.dialog.open(DeleteConfirmModalComponent, {
+      width: '500px',
+      data: {
+        heading: 'Delete Product',
+        msg: 'Are you sure you want to delete',
+        name: prod.productName,
+      },
+    });
 
-    openViewDialog(prod:any): void {
-      localStorage.setItem('productId',prod._id);
-      const dialogRef = this.dialog.open(ProductDetailsComponent, {
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed',result);
-        if(result){
-          this.deleteProduct(this.selectedID);
-        }
-      });
-    }
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+      if (result) {
+        this.deleteProduct(this.selectedID);
+      }
+    });
+  }
 
+  openViewDialog(prod: any): void {
+    localStorage.setItem('productId', prod._id);
+    const dialogRef = this.dialog.open(ProductDetailsComponent, {});
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed', result);
+      if (result) {
+        this.deleteProduct(this.selectedID);
+      }
+    });
+  }
 }
